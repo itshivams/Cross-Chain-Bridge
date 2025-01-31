8b6fa4248d9210949900c673a07c3ca63f0f38f0
Shivam <143623476+myselfshivams@users.noreply.github.com>
2025-02-01 04:07:05 +0530

    added:: signature

--- contracts/src/ChainAGateway.sol
+++ contracts/src/ChainAGateway.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -7,7 +8,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 interface IChainGateway {
     event Locked(address indexed user, uint256 amount, uint256 timestamp, uint256 nonce);
     event Unlocked(address indexed user, uint256 amount, uint256 timestamp);
-
+    
     function lock(uint256 amount, uint256 nonce, bytes calldata signature) external;
     function unlock(uint256 amount) external;
 }
@@ -16,17 +17,38 @@ contract ChainAGateway is IChainGateway, Ownable, ReentrancyGuard {
     IERC20 public tokenA;
     address public chainBGateway;
     mapping(uint256 => bool) public processedNonces;
+    address public trustedSigner;
 
-    constructor(address _tokenA) {
+    constructor(address _tokenA, address _trustedSigner) {
         tokenA = IERC20(_tokenA);
+        trustedSigner = _trustedSigner;
     }
 
     function setChainBGateway(address _chainBGateway) external onlyOwner {
         chainBGateway = _chainBGateway;
     }
 
+    function verifySignature(address user, uint256 amount, uint256 nonce, bytes calldata signature) internal view returns (bool) {
+        bytes32 messageHash = keccak256(abi.encodePacked(user, amount, nonce, address(this)));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(signature);
+        address recoveredSigner = ecrecover(ethSignedMessageHash, v, r, s);
+        return recoveredSigner == trustedSigner;
+    }
+
+    function splitSignature(bytes memory sig) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(sig.length == 65, "Invalid signature length");
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+    }
+
     function lock(uint256 amount, uint256 nonce, bytes calldata signature) external override nonReentrant {
         require(!processedNonces[nonce], "Nonce already used");
+        require(verifySignature(msg.sender, amount, nonce, signature), "Invalid signature");
         require(tokenA.transferFrom(msg.sender, address(this), amount), "Transfer failed");
         processedNonces[nonce] = true;
         emit Locked(msg.sender, amount, block.timestamp, nonce);
--- contracts/src/ChainBGateway.sol
+++ contracts/src/ChainBGateway.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -7,7 +8,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 interface IChainGateway {
     event Minted(address indexed user, uint256 amount, uint256 timestamp);
     event Burned(address indexed user, uint256 amount, uint256 timestamp);
-
+    
     function mint(address to, uint256 amount, uint256 nonce, bytes calldata signature) external;
     function burn(address from, uint256 amount) external;
 }
@@ -16,9 +17,11 @@ contract ChainBGateway is IChainGateway, Ownable, ReentrancyGuard {
     IERC20 public tokenB;
     address public chainAGateway;
     mapping(uint256 => bool) public processedNonces;
+    address public trustedSigner;
 
-    constructor(address _tokenB) {
+    constructor(address _tokenB, address _trustedSigner) {
         tokenB = IERC20(_tokenB);
+        trustedSigner = _trustedSigner;
     }
 
     function setChainAGateway(address _chainAGateway) external onlyOwner {
@@ -27,9 +30,28 @@ contract ChainBGateway is IChainGateway, Ownable, ReentrancyGuard {
 
     function mint(address to, uint256 amount, uint256 nonce, bytes calldata signature) external override onlyOwner nonReentrant {
         require(!processedNonces[nonce], "Nonce already used");
+        require(verifySignature(to, amount, nonce, signature), "Invalid signature");
         processedNonces[nonce] = true;
         tokenB.transfer(to, amount);
         emit Minted(to, amount, block.timestamp);
+    }
+
+    function verifySignature(address user, uint256 amount, uint256 nonce, bytes calldata signature) internal view returns (bool) {
+        bytes32 messageHash = keccak256(abi.encodePacked(user, amount, nonce, address(this)));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(signature);
+        address recoveredSigner = ecrecover(ethSignedMessageHash, v, r, s);
+        return recoveredSigner == trustedSigner;
+    }
+
+    function splitSignature(bytes memory sig) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(sig.length == 65, "Invalid signature length");
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
     }
 
     function burn(address from, uint256 amount) external override onlyOwner nonReentrant {
